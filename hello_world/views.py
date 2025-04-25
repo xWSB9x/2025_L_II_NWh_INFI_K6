253d04f67a1eac156aa30514ec9fd8caf7d34b83
xWSB9x <xwsb9x@gmail.com>
2025-04-25 16:53:32 +0200

    Naprawiono błędy składni, poprawiono identację i dodano Dockerfile

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Natalia"
+moje_imie = "Wiktor"
 msg = "Hello World!"
 
 
@@ -12,8 +12,7 @@ def index():
     output = request.args.get('output')
     if not output:
         output = PLAIN
-    return get_formatted(msg, moje_imie,
-                         output.lower())
+    return get_formatted(msg, moje_imie), output.lower()
 
 
 @app.route('/outputs')
@@ -21,6 +20,5 @@ def supported_output():
     return ", ".join(SUPPORTED)
 
 
-ddef blad():
+def blad():
     print("To blad")
-    
